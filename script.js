0d9add3c08a7173828a879e0d92b8333f0ec0ea0
luisferreiraaugusto-lab <luis.ferreira.augusto@escola.pr.gov.br>
2026-08-24 12:34:08 -0300

    s

--- script.js
+++ script.js
@@ -54,5 +54,14 @@ const perguntas = [
     alternativas: ["Alternativa 1", "Alternativa 2"],
   },
 
-
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
