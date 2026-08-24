001292b95247cb6af7e07c615fba69d905f29ffd
luisferreiraaugusto-lab <luis.ferreira.augusto@escola.pr.gov.br>
2026-08-24 12:32:09 -0300

    aula 3 personalizada

--- script.js
+++ script.js
@@ -6,53 +6,43 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
   {
-    enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "1. Você tem mais amigos da sua idade ou com uma faixa etária muito diferente da sua?",
+    alternativas: ["a) Sim", "b) Não"],
   },
   {
-    enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "2. O etarismo (ou preconceito de idade) costuma criar barreiras sociais que dificultam a aproximação e a amizade entre pessoas de gerações diferentes?",
+    alternativas: ["a) Verdadeiro", "b) Falso"],
   },
 
   
     {
-    enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "3. Amizades que cruzam barreiras de idade (intergeracionais) trazem benefícios mútuos, permitindo a troca de experiências e a quebra de preconceitos etários?",
+    alternativas: ["a) Falso", "b) Verdadeiro"],
   },
   {
-    enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "4. Afirmar rigidamente que 'jovens só devem andar com jovens e velhos só com velhos' é uma ideia que reforça a segregação imposta pelo etarismo na sociedade?",
+    alternativas: ["a) Verdadeiro", "b) Falso"],
   },
 
 
     {
-    enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "5. O lema 'para as amizades, as idades não importam' desafia a lógica do etarismo ao valorizar a conexão humana e a afinidade pessoal acima de rótulos geracionais?",
+    alternativas: ["a) Falso", "b) Verdadeiro"],
   },
   {
-    enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-  },
-
-
-    {
-    enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-  },
-  {
-    enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-  },
-
-
-    {
-    enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-  },
-  {
-    enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "6. O incentivo a atividades comunitárias que integram diferentes faixas etárias ajuda a reduzir a solidão e a construir uma sociedade menos egoísta?",
+    alternativas: ["a) Verdadeiro", "b) Falso"],
   },
 
 
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
